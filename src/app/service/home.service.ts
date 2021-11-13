9d63a5fd2565656eb97a70afd44f8edf0d6501eb
Omar Rahwanji <rahwanjiomar@gmail.com>
2021-11-13 11:05:37 +0200

    commented out the toastr

--- src/app/service/home.service.ts
+++ src/app/service/home.service.ts
@@ -17,12 +17,12 @@ export class HomeService {
     this.spinner.show();
     this.http.get('http://localhost:3456/api/WebsiteInfo/GetWebsiteInfoByRoleId/2').subscribe((result: any) => {
       this.webPageData = result;
-      this.toastr.success('Data Retrieved Successfuly 😁');
+      // this.toastr.success('Data Retrieved Successfuly 😁');
       this.spinner.hide();
     },
       error => {
         this.spinner.hide();
-        this.toastr.error('Failed Retrieving Data 😐');
+        // this.toastr.error('Failed Retrieving Data 😐');
       }
     );
   }
